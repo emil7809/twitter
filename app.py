83a06fd5ad77f930a8cb27da50c467d3e1b57fb5
emily <emil7809@gmail.com>
2023-02-27 18:57:23 +0100

    work please

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ trends = [
 
 @post('/secret_url_for_git_hook')
 def git_update():
-  repo = git.Repo('./twitter')
+  repo = git.Repo('./mysite')
   origin = repo.remotes.origin
   repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
   origin.pull()
@@ -87,4 +87,4 @@ try:
   application = default_app()
 except Exception as ex:
   print("Running local server")
-  run(host="127.0.0.1", port=3000, debug=True, reloader=True)
+  run(host="127.0.0.1", port=80, debug=True, reloader=True)
